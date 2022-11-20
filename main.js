dd8cb9d8853152a4033ddd50f07cd3b4f8723417
NikoHOME <wiktor.gawronski@gmail.com>
2022-11-20 17:37:05 +0100

    Update

--- main.js
+++ main.js
@@ -1,84 +1,66 @@
 
-function changeToDark()
-{
-    $('.edit').each(function(i, el) {
-        var classList = $(el).attr("class")
-        var classArr = classList.split(/\s+/);
-        classArr[0]=classArr[0]+"-dark";
-        $(el).removeClass();
-        for(const i in classArr)
-        {
-            $(el).addClass(classArr[i]);
-        }
-        $('#modeImg').attr('src', 'img/moon.png');
-    });
-}
-
-function changeToLight()
-{
-    $('.edit').each(function(i, el) {
-        var classList = $(el).attr("class")
-        var classArr = classList.split(/\s+/);
-        classArr[0]=classArr[0].slice(0,-5);
-        $(el).removeClass();
-        for(const i in classArr)
-        {
-            $(el).addClass(classArr[i]);
-        }
-        $('#modeImg').attr('src', 'img/sun.png');
-    });
-}
 
 function manageModes()
 {
-    if(window.localStorage)
+    if(window.localStorage.isDark=="true")
     {
-        if(window.localStorage.isDark=="true")
+        swapCSS(true);
+        window.localStorage.isDark="false";
+    }
+    else
+    {
+        swapCSS(false);
+        window.localStorage.isDark="true";
+    }
+
+}
+
+
+
+
+function onLoad()
+{
+    if(window.localStorage.isDark=="true") 
+    {
+        if( !($( "#checkBox" ).is(":checked")))  
         {
-            if(checkMode($('body'))) changeToLight();
-            window.localStorage.isDark="false";
-        }
-        else
-        {
-            if(!checkMode($('body'))) changeToDark();
+            document.getElementById('checkBox').click();
             window.localStorage.isDark="true";
+            swapCSS(false);
+
+        }
+    }
+    else
+    {
+        if( ($( "#checkBox" ).is(":checked"))) 
+        {
+            document.getElementById('checkBox').click();
+            window.localStorage.isDark="false";
+            swapCSS(true);
         }
     }
 
 }
 
-function checkMode(el)
-{
-    var classList = $(el).attr("class")
-    var classArr = classList.split(/\s+/);  
-    if(classArr[0]=="body") return false;
-    else return true;
-}
 
-
-function onLoad()
+function swapCSS(cond)
 {
-    if(window.localStorage)
+    var jqrStyle = $("[id^=jqr]");
+    var cssStyle = $("[id^=css]");
+    if(cond==true) 
     {
-
-        if(window.localStorage.isDark=="true") 
-        {
-            changeToDark();
-            if( !($( "#checkBox" ).is(":checked")))  
-            {
-                document.getElementById('checkBox').click();
-                window.localStorage.isDark="true";
-                changeToDark();
-            }
-        }
-        else
-        {
-            if( ($( "#checkBox" ).is(":checked"))) 
-            {
-                document.getElementById('checkBox').click();
-                window.localStorage.isDark="false";
-                changeToLight();
-            }
-        }
+        jqrStyle.attr('href', "css/custom-light.css");
+        cssStyle.attr('href', "css/style-light.css");
+        jqrStyle.attr('id', "jqrDark");
+        cssStyle.attr('id', "cssDark");
+        $('#modeImg').attr('src', 'img/sun.png');
+    }
+    else 
+    {
+        jqrStyle.attr('href', "css/custom-dark.css");
+        cssStyle.attr('href', "css/style-dark.css");
+        jqrStyle.attr('id', "jqrLight");
+        cssStyle.attr('id', "cssLight");
+        $('#modeImg').attr('src', 'img/moon.png');
     }
 }
